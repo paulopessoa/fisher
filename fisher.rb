daceaa31bbb8e63c07c3f970f5eb8abcf5402a34
Paulo Pessoa <dsm-sigsaude@dsm.mar.mil.br>
2016-10-10 16:20:58 -0300

    tratamento de erros/exceções e refatoração de métodos

--- fisher.rb
+++ fisher.rb
@@ -11,43 +11,33 @@ require 'open-uri'
 module Fisher
 
   PATH_TO = "#{Dir.pwd}/fisher_files/";
+  IMG_REGEX = /<img.*src=\"(.*?)\"/;
 
   def self.fish_file(url)
     # url path de um arquivo
-    if save_url(url, nil)
+    if save_url(url)
       puts "Arquivo encontrado: '#{url}'"
     else
       puts "Nenhum arquivo encontrado."
     end
-  rescue Exception => e
-    puts e.message
-    # puts e.backtrace.inspect
   end
 
 
   # Pega imagens de uma pagina a partir da URL
   def self.fish_images(url)
-    contents = URI.parse(url).read
-    # puts contents
-    i=0
-    contents.scan(/(<img.*src=\")(.*)\">/) do |tag, path|
-      # puts path
-      filename = path[path.rindex('/')+1..-1]
-      save_url(path, filename)
-      i+=1
+    fish(url, IMG_REGEX) do |path|
+      if save_url(path)
+        puts "***Peguei :)"
+      else
+        puts "***Não peguei :|"
+      end
     end
-    puts "#{i} arquivos encontrados."
-  rescue Exception => e
-    puts e.message
-    # puts e.backtrace.inspect
   end
 
 
   # Pega imagens de um css
   def self.fish_css(url)
-    contents = URI.parse(url).read
-    i=0
-    contents.scan(/url\(\"?(.*)\"?\)/) do |path|
+    fish(url, /url\(\"?(.*)\"?\)/) do |path|
       path = path.to_s.gsub("\"","").gsub("[","").gsub("]","").gsub("\\","")
       path = "http://www.stjunior.stj.jus.br#{path.to_s}"
 
@@ -55,10 +45,6 @@ module Fisher
       save_url(path, filename)
       i+=1
     end
-    puts "#{i} arquivos encontrados."
-  rescue Exception => e
-    puts e.message
-    puts e.backtrace.inspect
   end
 
   # Pega conteudo de links
@@ -84,11 +70,27 @@ module Fisher
     puts e.backtrace.inspect
   end
 
-
   private
 
+  def self.fish(url, pattern_to_find)
+    contents = URI.parse(url).read
+    i=0
+    contents.scan(pattern_to_find) do |path|
+      yield path.join
+      i+=1
+    end
+    puts "#{i} arquivos encontrados."
+  rescue Exception => e
+    puts e.message
+    puts e.backtrace.inspect
+  end
+
   # Salva arquivo a partir da URL
-  def self.save_url(url, filename)
+  def self.save_url(url)
+    puts "URL:#{url}"
+
+    filename = url[url.rindex('/')+1..-1] #nome do arquivo no path
+    # puts "FILENAME:#{filename}"
 
     if !File::directory?(PATH_TO)
       if !FileUtils.mkdir_p(PATH_TO)
@@ -104,35 +106,42 @@ module Fisher
 
     open(url) do |io|
       if filename.nil?
-        if !io.meta["content-disposition"].nil?
-          filename = io.meta["content-disposition"].scan(/filename=\"(.*)\"/)[0][0]
-        else
-          puts "URL #{url} não é um arquivo."
-          return false
-        end
+        puts "URL não é um arquivo."
+        return false
       end
       filename = "#{PATH_TO}#{filename}"
 
-      File.open(filename,"wb") do |f|
-        # if f.size > 0
-          f.puts io.read
-        # else
-        #   false
-        # end
+      if !save_file(io, filename)
+        false
       end
     end
-
-    puts "Peguei #{filename}"
     true
+
   rescue Exception => e
-    puts "Erro salvando URL #{url}: #{e.message}"
+    puts "[ERROR] Erro salvando URL: #{e.message}"
     e.backtrace.inspect
+    false
+  end
+
+  def self.save_file(io, filename)
+    File.open(filename,"wb") do |f|
+      # if f.size > 0
+        f.puts io.read
+      # else
+      #   false
+      # end
+    end
+    true
+
+  rescue Exception => e
+    puts "[ERROR] Erro salvando arquivo: #{e.message}"
+    e.backtrace.inspect
+    false
   end
 
 end
 
 # PRINCIPAL
-# Fisher.fish_file "http://www.stjunior.stj.jus.br/portal_stj/admin/fotografias/cad_fotografias_download.wsp?tmp.id=334&tmp.tamanho=amostra"
-# Fisher.fish_images "http://www.stjunior.stj.jus.br/portal_stj/publicacao/engine.wsp?tmp.area=1094"
-# Fisher.fish_css "http://www.stjunior.stj.jus.br/portal_stj/site/css/18_default.css"
-# Fisher.fish_css "http://www.stjunior.stj.jus.br/portal_stj/site/css/14_default.css"
+# Fisher.fish_images "http://dsm.mb/organograma"
+Fisher.fish_file "http://dpmm.mb/bpm/bpm172016.pdf"
+Fisher.fish_file "http://dpmm.mb/bpm/bpm182016.pdf"
